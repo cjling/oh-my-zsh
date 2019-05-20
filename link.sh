d445992ddb34a21f012bfdc73e256e0a96f4ebfb
cjling <cjling@aliyun.com>
2019-05-20 11:19:04 +0800

    fix_dev_branch

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@ oh_path=$(pwd)
 
 rm ~/.zshrc
 rm ~/.oh-my-zsh
+rm ~/.zsh_history
 
 ln -sf $oh_path/templates/zshrc.cjling      ~/.zshrc
+ln -sf $oh_path/templates/zsh_history       ~/.zsh_history
 ln -sf $oh_path                             ~/.oh-my-zsh
---
+++ plugins/stap/stap.plugin.zsh
@@ -0,0 +1,7 @@
+alias sl="stap -L"
+alias svl="stap -vv -L"
+
+alias se="stap -vv -e"
+
+alias sv="stap -vv"
+alias svg="stap -vv -g"
